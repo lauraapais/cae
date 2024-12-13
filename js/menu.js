a752c56eb796894e11e16860f3304122efde729c
Laura Pais <pais.laura@gmail.com>
2024-12-13 09:41:56 +0000

    ++

--- js/menu.js
+++ js/menu.js
@@ -21,3 +21,30 @@ document.getElementById('openMenu').addEventListener('click', function() {
     this.style.display = 'none';
     openButton.style.display = 'block';
   });
+
+
+
+  const logo = document.querySelector('.simpleLogoHeader');
+
+  logo.addEventListener('mouseenter', () => {
+      gsap.to("#Rectangle_309", { x: 5, duration: 0.5 });
+      gsap.to("#Rectangle_310", { x: 15, duration: 0.5 });
+      gsap.to("#Rectangle_311", { x: 35, duration: 0.5 });
+      gsap.to("#Rectangle_312", { x: 55, duration: 0.5 });
+      gsap.to("#Rectangle_313", { x: 0, y: 0, duration: 0.5 });
+      gsap.to("#Rectangle_314", { x: 25, duration: 0.5 });
+      gsap.to("#Rectangle_315", { x: 30, duration: 0.5 });
+      gsap.to("#Rectangle_316", { x: 45, duration: 0.5 });
+  });
+  
+  logo.addEventListener('mouseleave', () => {
+      gsap.to("#Rectangle_309", { x: 0, duration: 0.5 });
+      gsap.to("#Rectangle_310", { x: 10, duration: 0.5 });
+      gsap.to("#Rectangle_311", { x: 30, duration: 0.5 });
+      gsap.to("#Rectangle_312", { x: 45, duration: 0.5 });
+      gsap.to("#Rectangle_313", { x: 5, y: 0, duration: 0.5 });
+      gsap.to("#Rectangle_314", { x: 20, duration: 0.5 });
+      gsap.to("#Rectangle_315", { x: 35, duration: 0.5 });
+      gsap.to("#Rectangle_316", { x: 50, duration: 0.5 });
+  });
+  
