2730bde7627703ee49702692853a75418a4e25b4
Laura Pais <pais.laura@gmail.com>
2024-12-13 14:37:52 +0000

    ++

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,35 @@
+/*Menu*/
 document.getElementById('openMenu').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    const closeIcon = document.getElementById('closeMenu');
-    
-    if (menu.classList.contains('show')) {
-      menu.classList.remove('show');
-      closeIcon.style.display = 'none';
-      this.style.display = 'block';
-    } else {
-      menu.classList.add('show');
-      closeIcon.style.display = 'block';
-      this.style.display = 'none';
-    }
-  });
+  const menu = document.getElementById('menu');
+  const closeIcon = document.getElementById('closeMenu');
   
-  document.getElementById('closeMenu').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    const openButton = document.getElementById('openMenu');
-    
+  if (menu.classList.contains('show')) {
     menu.classList.remove('show');
+    closeIcon.style.display = 'none';
+    this.style.display = 'block';
+  } else {
+    menu.classList.add('show');
+    closeIcon.style.display = 'block';
     this.style.display = 'none';
-    openButton.style.display = 'block';
-  });
+  }
+});
+
+document.getElementById('closeMenu').addEventListener('click', function() {
+  const menu = document.getElementById('menu');
+  const openButton = document.getElementById('openMenu');
+  
+  menu.classList.remove('show');
+  this.style.display = 'none';
+  openButton.style.display = 'block';
+});
+
 
 
 
   const logo = document.querySelector('.simpleLogoHeader');
+  const headerDiv = document.querySelector('.headerDiv');
 
-  logo.addEventListener('mouseenter', () => {
+  headerDiv.addEventListener('mouseenter', () => {
       gsap.to("#Rectangle_309", { x: 5, duration: 0.5 });
       gsap.to("#Rectangle_310", { x: 15, duration: 0.5 });
       gsap.to("#Rectangle_311", { x: 35, duration: 0.5 });
