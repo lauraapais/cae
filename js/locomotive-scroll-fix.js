e5708a8d9aa16261cdad81c4b73fc6278cb377e8
Laura Pais <pais.laura@gmail.com>
2024-12-18 11:36:29 +0000

    ++

--- js/locomotive-scroll-fix.js
+++ js/locomotive-scroll-fix.js
@@ -1,50 +1,6 @@
-var scroller;
-
-window.addEventListener('load', (event) => {
-    scroller = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
-    //scroller.init();
-    scroller.start();
-
-    let loadingElements = document.querySelectorAll('.loading');
-    loadingElements.forEach(element => {
-        element.classList.remove('loading');
-        
-    });
+var scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
 });
 
-//scroller.stop();
-
-
-window.onload = function () {
-    scroller.update();
-}
-
-
-const anchorLinks = document.querySelectorAll('a[href^="#"]');
-anchorLinks.forEach(anchor => {
-    anchor.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetID = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetID);
-
-        if (targetElement) {
-            scroller.scrollTo(targetElement);
-        }
-    });
-});
-
-
-function scrollerMenu() {
-    if (!menu.classList.contains("active")) {
-        scroller.start();
-        onScrollMenu(scroller.scroll.instance.scroll.y);
-    } else {
-        scroller.stop();
-        if (nav.classList.contains("scroll")) {
-            nav.classList.remove("scroll");
-        }
-    }
-}
+new ResizeObserver(() => scroller.update()).observe(document.querySelector("[data-scroll-container]"));
